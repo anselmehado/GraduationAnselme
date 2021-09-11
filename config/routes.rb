45ab28e52af508b6cf9cfd9cfcac0415cf9cdb61
hado anselme <anselmehado85@yahoo.com>
2021-09-11 11:58:20 +0100

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  root "publications#index"
   resources :favorites
   resources :publications
   resources :comments
   resources :users
   resources :categories
+  root "publications#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_11_045908) do
+ActiveRecord::Schema.define(version: 2021_09_11_105723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,8 +47,8 @@ ActiveRecord::Schema.define(version: 2021_09_11_045908) do
     t.date "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id"
     t.bigint "category_id"
+    t.bigint "user_id"
     t.index ["category_id"], name: "index_publications_on_category_id"
     t.index ["user_id"], name: "index_publications_on_user_id"
   end
